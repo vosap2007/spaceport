543fa0b9d93695d946975de4c6b0991101d3de4f
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-10-27 14:31:08 +0300

    adds fonts

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg';
+import header_logo from '../../assets/images/header_logo.svg';
 
 const Header = () => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <ReactSVG src={header_logo} className="header__logo" />
+      <div className="header__registration">
+        <p className="header__account">motzart.near</p>
+        <button
+          className="header__sign_out"
+          onClick={() => {
+            console.log('Sign out click!');
+          }}
+        >
+          Sign out
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
