7091aafc50173cd6f6027b0c940b5411e36ac1f2
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-10-31 10:00:05 +0200

    adds Undock

--- src/pages/HomePage/components/Undock/index.js
+++ src/pages/HomePage/components/Undock/index.js
@@ -2,9 +2,46 @@ import React from 'react';
 
 const Undock = () => {
   return (
-    <>
-      <h1>Undock content</h1>
-    </>
+    <div className="undock">
+      <h1 className="undock__title">Undocking Your SPACEPORT Account</h1>
+      <p className="undock__text">
+        SPACEPORT allows users to connect to dApps and services in the NEAR
+        ecosystem from a secure and simple entry point. However, once you are
+        ready to fly on your own you may UNDOCK your SPACEPORT account and head
+        out on your own!
+      </p>
+
+      <ul className="undock__following">
+        Effectively you will be doing the following:
+        <li>Creating your own NEAR wallet</li>
+        <li>Transferring your assets to the wallet</li>
+        <li>
+          Launching away from SPACEPORT to explore on your own- this means you
+          will be responsible for your PRIVATE KEY. <br />{' '}
+          <p className="undock__following__happy">
+            No one will be able to help you recover your wallet if anything
+            happens!
+          </p>
+        </li>
+      </ul>
+
+      <h2 className="undock__title2">So are you ready to fly alone?</h2>
+      <ul className="undock__launch">
+        Launch Sequence:
+        <li>Initiate and fund launch</li>
+        <li>Transferring of assets</li>
+        <li>Confirm undock</li>
+      </ul>
+
+      <button
+        className="undock__button"
+        onClick={() => {
+          console.log('click button Proceed to Terms & Conditions');
+        }}
+      >
+        Proceed to Terms & Conditions
+      </button>
+    </div>
   );
 };
 
